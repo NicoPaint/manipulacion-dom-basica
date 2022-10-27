2d3f7aa51d971f06ba214ee23429b692f0c71fcd
Nicolas Pinto <felipepinve@gmail.com>
2022-10-26 22:39:54 -0500

    Se utilizo el atributo onclick para ver un manera basica de interaccion entre el html y el javascript

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 //existen funciones como querySelector que permite acceder a elementos del html desde el el documento JavaScript
 
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
-const input = document.querySelector("input")
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-})
+//esta funcion se ejecuta cada vez que se oprime el boton por el atributo onclick que se aplico en la etiqueta button
+function btnOnClick() {
+    console.log("Hola a todos, me oprimieron");
+    console.log(input1.value + input2.value);
+    console.log(Number(input1.value) + Number(input2.value));
 
-h1.innerHTML = "Nicolas eres el mejor"; //convierte todo a lenguaje HTML dentro de la etiqueta h1
-h1.innerText = "Nicolas eres el mejor"; //convierte todo a texto dentro de la etiqueta de h1
-
-console.log(h1.getAttribute("class")); //getAttribute captura el valor del atributo que le pasamos
-h1.setAttribute("class", "rojo"); //setAttribute modifica el valor del atributo que le pasamos. primero el atributo y luego el nuevo valor
-
-//para trabajar mejor con clase existe classList. Esta tiene diferentes metodos para trabajar con las clases
-h1.classList.add("azul");
-h1.classList.remove("rojo");
-//h1.classList.toggle("rojo");
-//h1.classList.contains("rojo");
-
-const img = document.createElement("img"); //con el metodo createElement se puede crear cualquier etiqueta de HTML que se quiera
-img.setAttribute("src", "https://static1.personality-database.com/profile_images/5872a218bb964d06bd5c92da1f2167fc.png");
-
-pid.append(img); //el metodo append o appendChild agrega lo que se le pase como contenido a la etiqueta que se le aplique
-
-
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
