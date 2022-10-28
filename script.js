a3a2a5da61e07825c9646868103272b19b35fc5f
Nicolas Pinto <felipepinve@gmail.com>
2022-10-27 21:38:46 -0500

    Se aplicó un ejemplo de addEventListener al boton que se tiene en el html

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult = document.querySelector("#result");
 
+btn.addEventListener("click", btnOnClick); //el metodo addEventListener permite manejar los eventos desde el JS meramente. Primero se pasa el tipo de evento que se quiere escuchar y segundo el codigo JS que se quiere ejecutar cuando sucede el evento.
+
 //esta funcion se ejecuta cada vez que se oprime el boton por el atributo onclick que se aplico en la etiqueta button
 function btnOnClick() {
     console.log("Hola a todos, me oprimieron");
